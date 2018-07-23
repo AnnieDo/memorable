06769258fcb41c59226c7a3649df045db87d2bdd
AnnieDo <39351910+AnnieDo@users.noreply.github.com>
2018-07-23 14:44:44 -0700

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do root 'cars#index'
+  root 'cars#index'
   resources :cars
-  resources :cars
-  # The priority is based upon order of creation: first created -> highest priority.
+end
+
+# The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
